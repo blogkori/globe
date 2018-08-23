1eed455d7881166ede0bcbdea9b86529f7a23faf
Atikur Rabbi <msawon94@gmail.com>
2018-08-24 02:15:35 +0600

    Update dataloading.js

--- js/dataloading.js
+++ js/dataloading.js
@@ -21,7 +21,7 @@ function loadWorldPins( callback ){
 }
 
 function loadContentData(callback){	
-	var filePath = "categories/sam.json";
+	var filePath = "categories/All.json";
 	filePath = encodeURI( filePath );
 	// console.log(filePath);
 			
